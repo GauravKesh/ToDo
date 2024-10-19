ab387857ee80dc24153be341e548d0239b009c6d
Gaurav Kesh Roushan <gkrcoder@gmail.com>
2024-10-19 19:57:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import Footer from './components/Footer';
 import Todo from './components/ToDo';
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 
 
 function App() {
@@ -10,8 +8,6 @@ function App() {
       <div className="bg-gray-900">
         <Todo />
         <Footer/>
-        <Analytics/>
-        <SpeedInsights/>
       </div>
     </>
   );
